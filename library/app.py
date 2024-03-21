4dd7ae5e2a288100116b16782d0f38fa1e9cd164
AlexandrDulovec <alexandr.dulovec@student.ossp.cz>
2024-03-21 09:58:30 +0100

    14.3. - library

--- library/app.py
+++ library/app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ def add_book():
     if request.method == 'POST':
         title = request.form['title']
         author = request.form['author']
-        save_to_file('books.txt', f"Nadpis knihy: {title}, Autor: {author}")
+        save_to_json('books.json', {'title': title, 'author': author})
         return 'Kniha byla přidána!'
     return render_template('add_book.html')
 
@@ -20,13 +21,31 @@ def add_user():
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
-        save_to_file('users.txt', f"Uživatelské jméno: {username}, Email: {email}")
+        save_to_json('users.json', {'username': username, 'email': email})
         return 'Uživatel přidán!'
     return render_template('add_user.html')
 
-def save_to_file(filename, data):
+@app.route('/books')
+def display_books():
+    books = load_from_json('books.json')
+    return render_template('books.html', items=books)
+
+@app.route('/users')
+def display_users():
+    users = load_from_json('users.json')
+    return render_template('users.html', items=users)
+
+def save_to_json(filename, data):
     with open(filename, 'a') as file:
-        file.write(data + '\n')
+        json.dump(data, file)
+        file.write('\n')
+
+def load_from_json(filename):
+    try:
+        with open(filename, 'r') as file:
+            return [json.loads(line) for line in file.readlines()]
+    except FileNotFoundError:
+        return []
 
 if __name__ == '__main__':
     app.run(debug=True)
